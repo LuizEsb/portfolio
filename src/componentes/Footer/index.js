6d657965dee644987fd2636e9824e00a5284e9be
LuizEsb <luizfelipe284@outlook.com>
2025-08-22 11:49:00 -0300

    feat: adiciona footer

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,9 +1,11 @@
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
+import { ReactComponent as MarcaRegistrada } from "assets/marca_registrada.svg";
 
 export default function Footer() {
-    return(
-        <footer>
-            asdadas
-        </footer>
-    )
+  return (
+    <footer className={styles.rodape}>
+      <MarcaRegistrada />
+      Desenvolvido por <strong>LuizEsb</strong>
+    </footer>
+  );
 }
